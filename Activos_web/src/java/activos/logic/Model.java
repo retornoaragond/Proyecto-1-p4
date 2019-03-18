a88c40a39a3ac5a012ed2867422757df01d03eb8
steve <steve@Steve-O>
2019-03-18 12:57:11 -0600

    Carpetas 1

---
+++ Activos_web/src/java/activos/logic/Model.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activos.logic;
+
+/**
+ *
+ * @author steve
+ */
+public class Model {
+    
+}
